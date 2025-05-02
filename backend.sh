c39c22fdb98144d42495b5fb9dcd321f2a0c0e7e
vot5kor <vot5kor@gmail.com>
2025-05-02 10:56:38 +0530

    expense shellscript files

--- backend.sh
+++ backend.sh
@@ -1,2 +1,88 @@
 #!/bin/bash
 
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+FILE_NAME=$(echo $0 | cut -d "." -f1  )
+echo "test: $FILE_NAME"
+TIMESTAMP=$(date  +%Y-%m-%d:%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$FILE_NAME-$TIMESTAMP.log"
+echo "test: $LOG_FILE_NAME" &>>$LOG_FILE_NAME
+
+VALIDATE()
+{
+             if [ $1 -ne 0 ]
+                then # not Installed
+                echo -e "$2 ... $R FAILURE $N"
+                exit 1
+                else
+                 echo -e "$2 ... $G SUCCESS $N"
+            fi
+}
+
+
+CHECK_ROOT(){
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    echo "test: $USERID"
+    exit 1 #other than 0
+fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Disabling Nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Nodejs20"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Nodejs"
+
+useradd expense &>>$LOG_FILE_NAME
+VALIDATE $? "Creating expense user"
+
+mkdir /app &>>$LOG_FILE_NAME
+VALIDATE $? "Creating app Directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Download the application code"
+
+cd /app
+VALIDATE $? "Goto app directoty"
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? " Unzip the backend "
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? " Installing Dependencies "
+#We need to setup a new service in systemd so systemctl can manage this service
+#Setup SystemD Expense Backend Service
+#vim /etc/systemd/system/backend.service
+#cp backend.service /etc/systemd/system/backend.service
+cp /home/ec2-user/expense-shellscript/backend.service /etc/systemd/system/backend.servic
+
+#Prepare MYSQL Schema
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing MYSQL Client"
+
+mysql -h mysql.tuktukride.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up transactions schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Demoan Reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Backend"
+
+systemctl start backend &>>$LOG_FILE_NAME
+VALIDATE $? "Start Backend"
+
+
